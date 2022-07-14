5215faf6518c44fe7c585fd7ccd1773f0ac4aa37
MctomSpdo <mctom.spdo@gmail.com>
2022-07-14 23:21:35 +0200

    format module-info.java

--- code/dbasy/src/main/java/module-info.java
+++ code/dbasy/src/main/java/module-info.java
@@ -6,10 +6,13 @@ module com.example.dbasy {
     requires java.sql;
 
     opens com.example.dbasy to javafx.fxml;
+    opens com.example.dbasy.ui to javafx.fxml;
+
     exports com.example.dbasy;
+
     exports com.example.dbasy.ui;
+
     exports com.example.dbasy.database;
     exports com.example.dbasy.database.invalid;
     exports com.example.dbasy.database.mysql;
-    opens com.example.dbasy.ui to javafx.fxml;
 }
