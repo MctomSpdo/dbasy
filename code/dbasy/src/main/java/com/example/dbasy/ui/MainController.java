ff11f032bd31b7a5a5eb1fc6ef8cf27ae5c623ce
MctomSpdo <mctom.spdo@gmail.com>
2022-07-15 15:09:11 +0200

    fix issue with multiple connections

--- code/dbasy/src/main/java/com/example/dbasy/ui/MainController.java
+++ code/dbasy/src/main/java/com/example/dbasy/ui/MainController.java
@@ -1,9 +1,11 @@
 package com.example.dbasy.ui;
 
 import com.example.dbasy.Resources;
+import com.example.dbasy.database.Table;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -43,6 +45,25 @@ public class MainController {
         refreshDBTree(true);
     }
 
+    /**
+     * Connection Menu for adding new Connection
+     * @param event ActionEvent
+     */
+    @FXML
+    public void cmConnNewConn(ActionEvent event) {
+        UiUtil.connectionDialog(resources);
+        refreshDBTree(false);
+    }
+
+    /**
+     * Refreshes the View (View -> Refresh Tree)
+     * @param event ActionEvent
+     */
+    @FXML
+    public void viewRefreshTree(ActionEvent event) {
+        refreshDBTree(false);
+    }
+
     public MainController(Resources resources) {
         resources.controller = this;
         this.resources = resources;
@@ -62,6 +83,7 @@ public class MainController {
      * @param doRefresh if Tables and contents should be refreshed
      */
     public void refreshDBTree(boolean doRefresh) {
+        trDatabases.getRoot().getChildren().clear();
         var databases = resources.connections;
         databases.forEach((database -> {
             var view = new DatabaseView(database);
@@ -78,31 +100,34 @@ public class MainController {
                 //if treeItem was opened and first element is Loading...
                 if(t1 || doRefresh) {
                     var firstItem = tablesItem.getChildren().get(0);
-                    if(doRefresh || firstItem == null || firstItem.getValue().equals(UiUtil.getLoadingTreeItem().getValue())) {
-                        //load Tables from Database (start new Thread due to database)
-                        (new Thread(() -> {
-                            try {
-                                var tableList = database.getTables();
-                                //set tables in Tree View
-                                Platform.runLater(() -> {
-                                    tablesItem.getChildren().clear();
-                                    tableList.forEach((table -> {
-                                        var tableItem = new TreeItem(table);
-                                        tablesItem.getChildren().add(tableItem);
-                                        //children of database table:
-                                        var columnsItem = UiUtil.getColumnTreeItem(table);
-                                        tableItem.getChildren().add(columnsItem);
-                                    }));
-                                });
-                            } catch (SQLException e) {
-                                resources.log.error("Could not load Tables from" + database, e);
-                                //add error Tree Item:
-                                Platform.runLater(() -> {
-                                    tablesItem.getChildren().add(UiUtil.getErrorTreeItem());
-                                });
-                            }
-                        })).start();
-                    }
+                    try {
+                        if(doRefresh || firstItem == null || firstItem.getValue().equals(UiUtil.getLoadingTreeItem().getValue())) {
+                            //load Tables from Database (start new Thread due to database)
+                            (new Thread(() -> {
+                                try {
+                                    var tableList = database.getTables();
+                                    //set tables in Tree View
+                                    Platform.runLater(() -> {
+                                        tablesItem.getChildren().clear();
+                                        tableList.forEach((table -> {
+                                            var tableItem = new TreeItem(table);
+                                            tablesItem.getChildren().add(tableItem);
+                                            //children of database table:
+                                            var columnsItem = UiUtil.getColumnTreeItem(table);
+                                            tableItem.getChildren().add(columnsItem);
+                                        }));
+                                    });
+                                } catch (SQLException e) {
+                                    resources.log.error("Could not load Tables from" + database, e);
+                                    //add error Tree Item:
+                                    Platform.runLater(() -> {
+                                        tablesItem.getChildren().add(UiUtil.getErrorTreeItem());
+                                    });
+                                }
+                            })).start();
+                        }
+                    } catch (ClassCastException ignored) {} //when first element gets changed to table after check and before execution
+
                 }
             });
 
