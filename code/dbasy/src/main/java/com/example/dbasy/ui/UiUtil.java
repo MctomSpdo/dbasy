c8e0fde905ef3600be8a45417c257aab5bc3521c
MctomSpdo <mctom.spdo@gmail.com>
2022-07-15 13:42:50 +0200

    clean up columnsTreeViewItem

--- code/dbasy/src/main/java/com/example/dbasy/ui/UiUtil.java
+++ code/dbasy/src/main/java/com/example/dbasy/ui/UiUtil.java
@@ -1,8 +1,11 @@
 package com.example.dbasy.ui;
 
+import com.example.dbasy.Main;
 import com.example.dbasy.Resources;
 import com.example.dbasy.database.Database;
+import com.example.dbasy.database.Table;
 import com.example.dbasy.database.invalid.InvalidDatabase;
+import javafx.application.Platform;
 import javafx.scene.control.TreeItem;
 
 import java.io.IOException;
@@ -10,7 +13,7 @@ import java.sql.SQLException;
 
 public class UiUtil {
     /**
-     * Shows the connection Dialog to the user and adds the database when successfull
+     * Shows the connection Dialog to the user and adds the database when successful
      * @param resources resources to add the db to (and log)
      * @return Database added db
      */
@@ -39,5 +42,32 @@ public class UiUtil {
 
     public static TreeItem getErrorTreeItem() {
         return new TreeItem("Error");
+    }
+
+    public static TreeItem getColumnTreeItem(Table table) {
+        var columnsItem = new TreeItem<>("columns");
+        columnsItem.getChildren().add(UiUtil.getLoadingTreeItem());
+        columnsItem.expandedProperty().addListener((observableValue1, aBoolean1, t11) -> {
+            if(t11) {
+                var firstColumnItem = columnsItem.getChildren().get(0);
+                if(firstColumnItem == null || firstColumnItem.getValue().equals(UiUtil.getLoadingTreeItem().getValue())) {
+                    (new Thread(() -> {
+                        try {
+                            var headers = table.getHeaders();
+                            Platform.runLater(() -> {
+                                columnsItem.getChildren().clear();
+                                headers.forEach((header) -> {
+                                    var columnItem = new TreeItem<>(header);
+                                    columnsItem.getChildren().add(columnItem);
+                                });
+                            });
+                        } catch (SQLException e) {
+                            Main.RESOURCES.log.error("Could not load Columns from Database: " + table.getSource(), e);
+                        }
+                    })).start();
+                }
+            }
+        });
+        return columnsItem;
     }
 }
